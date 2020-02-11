b620d042667ccd8c1ecf1f0b60ff62d9fdeb9c4d
rangwei <rangwei@gmail.com>
2020-02-11 18:41:19 +0800

    first

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerModule } from './customer/customer.module';
+
+
+const url = process.env.MONGO_URL || 'localhost';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(`mongodb://${url}:27017/customer`, { useNewUrlParser: true })
+    , CustomerModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.enableCors(); // add this line
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(8080);
+}
+bootstrap();
